9af1db8ed526873ae2c8dfc5d549278da386b17e
lchanona <87346411+lchanona@users.noreply.github.com>
2024-10-21 17:28:16 -0600

    feature first example of using components

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Hi from "./components/Hi";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <header>
+        <nav style={{ display: "flex", justifyContent: "space-between" }}>
+          <p>logo</p>
+          <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
+            <li>
+              {" "}
+              <a href="#"> Home </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="#"> Service </a>{" "}
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Hi> </Hi>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi nihil
+          quo laborum dignissimos perspiciatis aliquid repudiandae! Distinctio
+          ut nesciunt voluptatibus dolorum voluptate, sequi laborum animi amet
+          id impedit qui ipsa.
+        </p>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ components/src/components/Hi.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hello() {
+  return <div style={{ background: "red" }}>Hello Everyone</div>;
+}
+
+export default Hello;
